736030bac163a2f4801e95a20c55e7d97d9b8759
Mykhailo Poliakov <mykhailo.poliakov@datarobot.com>
2024-08-06 15:14:59 +0300

    Use MultiHop-RAG QA evaluation logic

--- evaluate_qa.py
+++ evaluate_qa.py
@@ -1,34 +1,50 @@
+"""Mirrors code in MultiHop-RAG/qa_evaluate.py"""
 import glob
+import importlib
 import json
 import os
-import string
+import sys
+
+os.environ["TQDM_DISABLE"] = "1"
+sys.stdout = open(os.devnull, 'w')
+multihop_rag_qa_evaluate = importlib.import_module("MultiHop-RAG.qa_evaluate")
+sys.stdout = sys.__stdout__
 
 output_path = "qa_output"
 
-
-def clean_answer(answer):
-    return (
-        answer.lower()
-        .strip()
-        .translate(str.maketrans(dict.fromkeys(string.punctuation)))
-    )
-
-
-def get_accuracy(filename):
-    with open(filename, "r") as file:
-        doc_data = json.load(file)
-
-    correct_answers = 0
-
-    for d in doc_data:
-        if clean_answer(d["gold_answer"]) in clean_answer(
-            d["model_answer"]
-        ) or clean_answer(d["model_answer"]) in clean_answer(d["gold_answer"]):
-            correct_answers += 1
-
-    return correct_answers / len(doc_data)
-
-
 print(f"Evaluate files in folder: {output_path}")
 for filename in glob.glob(os.path.join(output_path, "*.json")):
-    print(f"For file: {filename} = {round(get_accuracy(filename), 4)}")
+    with open(filename, "r") as file:
+        doc_data = json.load(file)
+    if not doc_data:
+        continue
+    print("-" * 100)
+    print("Filename:", filename)
+    overall_pred_list = []
+    overall_gold_list = []
+    type_data = {}
+    # Main loop, iterate through document data
+    for d in doc_data:
+        model_answer = d["model_answer"]
+        gold = d["gold_answer"]
+        if gold:
+            question_type = d["question_type"]
+            if question_type not in type_data:
+                type_data[question_type] = {"pred_list": [], "gold_list": []}
+            type_data[question_type]["pred_list"].append(model_answer)
+            type_data[question_type]["gold_list"].append(gold)
+            overall_pred_list.append(model_answer)
+            overall_gold_list.append(gold)
+    for question_type, data in type_data.items():
+        precision, recall, f1 = multihop_rag_qa_evaluate.calculate_metrics(
+            data["pred_list"], data["gold_list"]
+        )
+        assert precision == recall == f1
+        print(f"Question Type: {question_type}. Accuracy: {precision:.4f}")
+
+    # Calculate overall evaluation metrics
+    overall_precision, overall_recall, overall_f1 = (
+        multihop_rag_qa_evaluate.calculate_metrics(overall_pred_list, overall_gold_list)
+    )
+    assert overall_precision == overall_recall == overall_f1
+    print(f"Overall Accuracy: {overall_precision:.4f}")
